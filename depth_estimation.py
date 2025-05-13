c01e6d5d37a031ef918b8258c09827584500ca6e
Emma Vejcik <47393221+evejcik@users.noreply.github.com>
2025-05-13 10:14:54 -0400

    starry night img

--- depth_estimation.py
+++ depth_estimation.py
@@ -17,8 +17,13 @@ model = build_model(config)
 model.eval().cuda()
 
 # 2. Load your style image
-img_path = "../gaussian_splatting/style/starrynight.jpg"
+# img_path = ".\gaussian-splatting\style\starrynight.jpg" # "C:\Users\cslabwin\gaussian-splatting\style\starrynight.jpg"
+# img_path = "./gaussian_splatting/style/starrynight.jpg"
+# img_path = r'C:\Users\cslabwin\gaussian_splatting\style\starrynight.jpg'
+img_path = '.\gaussian-splatting\style\starrynight.jpg'
+
 image = Image.open(img_path).convert("RGB")
+# image = Image.fromarray(colorize(image))I
 transform = Compose([Resize((512, 512)), ToTensor()])
 img_tensor = transform(image).unsqueeze(0).cuda()  # shape: [1, 3, H, W]
 
@@ -31,6 +36,8 @@ np.save("style_depth.npy", pred.cpu().numpy())
 print("Saved style_depth.npy")
 
 # 5. Save preview (optional)
-depth_colored = colorize(pred).convert("RGB")
+# depth_colored = colorize(pred).convert("RGB")
+depth_colored_np = colorize(pred)  # Returns numpy array
+depth_colored = Image.fromarray(depth_colored_np)
 depth_colored.save("style_depth_preview.png")
 print("Saved style_depth_preview.png")
